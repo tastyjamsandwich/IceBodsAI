243d299648e1057bd78645a5ddd2ed7cf2396588
tastyjamsandwich <114577433+tastyjamsandwich@users.noreply.github.com>
2024-10-10 11:29:12 +0200

    Update back-office.tsx

--- pages/back-office.tsx
+++ pages/back-office.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { 
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { ExcelHandler } from '@/components/ExcelHandler'
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Label } from "@/components/ui/label"
+import { ExcelHandler } from '@/components/ExcelHandler'
 
 interface Product {
   id: string
