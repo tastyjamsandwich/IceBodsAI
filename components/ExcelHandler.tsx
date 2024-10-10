04603b9e5ff9bfe5572faa6fb74b3d112ac7e188
tastyjamsandwich <114577433+tastyjamsandwich@users.noreply.github.com>
2024-10-10 11:28:00 +0200

    Update ExcelHandler.tsx

--- components/ExcelHandler.tsx
+++ components/ExcelHandler.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { ExcelHandler } from '@/components/ExcelHandler'
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-import { Label } from "@/components/ui/label"
+import { 
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 interface Product {
   id: string
