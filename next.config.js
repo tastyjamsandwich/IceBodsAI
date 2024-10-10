76c9f8f6b4f081a1a56a622c3943fc802193d482
tastyjamsandwich <114577433+tastyjamsandwich@users.noreply.github.com>
2024-10-10 12:12:06 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig
