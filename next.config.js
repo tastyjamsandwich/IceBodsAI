d1abf7b7351b37606627f78f7bb39434ce0b2675
tastyjamsandwich <114577433+tastyjamsandwich@users.noreply.github.com>
2024-10-11 12:21:14 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, 'prisma', '@prisma/client']
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
