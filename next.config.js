fb51928e67c6ef8b9a988eca3a92f69d37574ebc
tastyjamsandwich <114577433+tastyjamsandwich@users.noreply.github.com>
2024-10-11 11:41:46 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  },
+  experimental: {
+    forceSwcTransforms: true,
+  }
 }
 
 module.exports = nextConfig
